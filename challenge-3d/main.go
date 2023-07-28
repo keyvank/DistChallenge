222a6e21b01e075095689161e5b26a72a742d7b2
Glyphack <sh.hooshyari@gmail.com>
2023-07-28 22:58:21 +0200

    Just a lock

--- challenge-3d/main.go
+++ challenge-3d/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -21,28 +22,41 @@ func main() {
 func getNode() *maelstrom.Node {
 	n := maelstrom.NewNode()
 	var messages []int
-	var neighbours []string
+	var neighboursFromTopology []string
+	mutex := sync.Mutex{}
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
-		message := int(body["message"].(float64))
-		_, exists := body["from_node"]
-		if !contain(message, messages) {
-			messages = append(messages, message)
-		}
-		if !exists {
-			err := broadcast(message, n)
-			if err != nil {
-				return err
+		err := func() error {
+			// without the lock messages can be lost
+			mutex.Lock()
+			defer mutex.Unlock()
+			message := int(body["message"].(float64))
+			if !contain(message, messages) {
+				messages = append(messages, message)
+				neighbours := removeString(n.NodeIDs(), n.ID())
+				if msg.Src != getLeader(n) {
+					err := broadcast(message, n, neighbours)
+					return err
+				}
 			}
+			return nil
+		}()
+		if err != nil {
+			return err
 		}
-		reply := map[string]any{
-			"type": "broadcast_ok",
+
+		_, hasId := body["msg_id"]
+		if hasId {
+			reply := map[string]any{
+				"type": "broadcast_ok",
+			}
+			return n.Reply(msg, reply)
 		}
-		return n.Reply(msg, reply)
+		return nil
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -63,7 +77,7 @@ func getNode() *maelstrom.Node {
 			return err
 		}
 		for _, node := range body["topology"].(map[string]any)[n.ID()].([]any) {
-			neighbours = append(neighbours, node.(string))
+			neighboursFromTopology = append(neighboursFromTopology, node.(string))
 		}
 		reply := map[string]any{
 			"type": "topology_ok",
@@ -74,47 +88,49 @@ func getNode() *maelstrom.Node {
 	return n
 }
 
-func broadcast(message int, n *maelstrom.Node) error {
-	allNodes := n.NodeIDs()
-	neighbours := removeString(allNodes, n.ID())
-	// leader := getLeader(n)
-	for _, nodeId := range neighbours {
-		body := map[string]any{
-			"type":      "broadcast",
-			"message":   message,
-			"from_node": true,
-		}
-
-		go func(nodeId string) {
-			success := false
-			for !success {
-				ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*2)
-				defer cancelFunc()
-				resp, err := n.SyncRPC(ctx, nodeId, body)
-				if err != nil {
-					errCode := resp.RPCError().Code
-					if errCode == 0 || errCode == 13 {
-						fmt.Fprintln(os.Stderr, "failed to send message ", message, "to node ", nodeId)
-					}
-					time.Sleep(time.Second)
-					continue
-				}
-				fmt.Fprintln(os.Stderr, "successfully sent message ", message, "to node ", nodeId)
-				success = true
-			}
-		}(nodeId)
-		fmt.Fprintln(os.Stderr, time.Now().UnixNano()/int64(time.Millisecond), "sent message to neighbour from ", n.ID())
+func broadcast(message int, n *maelstrom.Node, neighbours []string) error {
+	leader := getLeader(n)
+	body := map[string]any{
+		"type":    "broadcast",
+		"message": message,
 	}
+	if n.ID() == leader {
+		// if leader send to all
+		for _, nodeId := range neighbours {
+			go sendMsgWithRetry(n, body, nodeId)
+		}
+	} else {
+		go sendMsgWithRetry(n, body, leader)
+	}
+
 	return nil
 }
 
-func getLeader(n *maelstrom.Node) string {
-	leader := ""
-	for _, nodeId := range n.NodeIDs() {
-		if nodeId > leader {
-			leader = nodeId
+func sendMsgWithRetry(n *maelstrom.Node, body map[string]any, dest string) {
+	success := false
+	for !success {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), time.Second*2)
+		defer cancelFunc()
+		resp, err := n.SyncRPC(ctx, dest, body)
+		if err != nil {
+			errCode := resp.RPCError().Code
+			if errCode == 0 || errCode == 13 {
+				fmt.Fprintln(os.Stderr, "failed to send message ", body, "to node ", dest)
+			}
+			time.Sleep(time.Second)
+			continue
 		}
+		success = true
 	}
+}
+func getLeader(n *maelstrom.Node) string {
+	leader := "n0"
+	// for _, nodeId := range n.NodeIDs() {
+	// 	if nodeId > leader {
+	// 		leader = nodeId
+	// 	}
+	// }
+	fmt.Fprintln(os.Stderr, "selected leader ", leader)
 	return leader
 }
 
